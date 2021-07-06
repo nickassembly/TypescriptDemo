dd6d30d62a95e8f6b166a6d8d6ab3c33e7eee8bf
nickguerra <nickg@maliciousgames.com>
2021-07-06 18:03:49 -0500

    added tuple examples

--- app.ts
+++ app.ts
@@ -1,12 +1,17 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
-    const person = {
+    const person: {
+        name: string;
+        age: number;
+        hobbies: string[];
+        role: [number, string];
+    } = {
     name: 'Nick',
     age: 42,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
+
+// person.role.push('admin');
+person.role = [0, 'admin'];
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
